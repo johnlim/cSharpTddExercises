0a22b63116883950ce3d73f9b6b8d4795c6eb540
John Lim <johnlim.dingwei@gmail.com>
2013-10-14 23:55:35 +0800

    Skip building NUnit if executable already exists.

--- makeNUnit.bat
+++ makeNUnit.bat
@@ -3,5 +3,14 @@ rem * Command line build - For CppUTest - Run from CppUTest directory
 rem * 
 rem * this path works on my machine
 rem ****PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /t:build /verbosity:quiet ..\NUnit\nunit.sln
 
+rem directory of executing batch file
+pushd %~dp0
+
+set nunit_console_exe=.\NUnit\bin\Debug\nunit-console.exe
+if exist %nunit_console_exe% goto SKIP
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /t:build /verbosity:quiet .\NUnit\nunit.sln
+
+:SKIP
+popd
