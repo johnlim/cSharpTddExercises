3deb4f048bc5433ca1166325edcc22ecdb755d19
John Lim <johnlim.dingwei@gmail.com>
2013-10-14 18:33:03 +0800

    Added NUnit build file and pre-build event to build NUnit.

---
+++ makeNUnit.bat
@@ -0,0 +1,7 @@
+rem ****
+rem * Command line build - For CppUTest - Run from CppUTest directory
+rem * 
+rem * this path works on my machine
+rem ****PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /t:build /verbosity:quiet ..\NUnit\nunit.sln
+
