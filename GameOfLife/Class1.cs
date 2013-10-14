f0fbbc145829e9081da636d8ee2eff7e5f0651af
John Lim <johnlim.dingwei@gmail.com>
2013-10-14 18:50:14 +0800

    World gives cells life correctly.

--- GameOfLife/Class1.cs
+++ GameOfLife/Class1.cs
@@ -5,14 +5,60 @@ using System.Text;
 
 namespace GameOfLife
 {
+  public class World
+  {
+    public World() { setOfLiveCells = new SortedSet<Cell>(); }
+    public void giveCellLife(Cell cell) { setOfLiveCells.Add(cell); }
+    public bool isCellAlive(Cell cell) { return setOfLiveCells.Contains(cell); }
+    private SortedSet<Cell> setOfLiveCells;
+  }
+
+  public class Cell : IComparable<Cell>
+  {
+    public Cell(int x, int y) { xCoordinate = x; yCoordinate = y; }
+    public int CompareTo(Cell cell)
+    {
+      if (xCoordinate < cell.xCoordinate) return -1;
+      if (xCoordinate > cell.xCoordinate) return 1;
+      if (xCoordinate == cell.xCoordinate && yCoordinate == cell.yCoordinate) return 0;
+      if (yCoordinate < cell.yCoordinate) return -1;
+      else return 1;
+    }
+    private int xCoordinate;
+    private int yCoordinate;
+  }
+}
+
+
+
+namespace ClassLibrary1
+{
   using NUnit.Framework;
-  
+  using GameOfLife;
+
+
   [TestFixture]
-  public class testFail
+  public class Class1
   {
     [Test]
-    public void testFailure(){
-      Assert.Ignore();
+    public void testWorldGivesCellLife()
+    {
+      World world = new World();
+      Cell cell = new Cell(0, 1);
+      world.giveCellLife(cell);
+      Assert.IsTrue(world.isCellAlive(cell));
+    }
+    [Test]
+    public void testWorldGivesAnotherCellLife()
+    {
+      Cell cell = new Cell(0, 0);
+      Cell anotherCell = new Cell(1, 0);
+      World world = new World();
+
+      world.giveCellLife(anotherCell);
+      Assert.IsFalse(world.isCellAlive(cell));
+      Assert.IsTrue(world.isCellAlive(anotherCell));
+
     }
   }
 }
